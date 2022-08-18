855929d8a4d3755940db353216f39d62a9452454
fepanbr <fepanbr@gmail.com>
2022-08-18 22:40:37 +0900

    add books reducer

---
+++ src/components/List.tsx
@@ -0,0 +1,15 @@
+export default function List() {
+  return (
+    <div>
+      <h1>
+        List
+        <button onClick={click}>logout</button>
+      </h1>
+    </div>
+    
+  )
+
+  function click() {
+    
+  }
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import ListContainer from "../containers/ListContainer";
 
 export default function Home() {
-  return (<div>
-    <h1>Home</h1>
-  </div>)
+  const token = useSelector<any , string | null> ((state) => state.auth.token)
+
+  if (token === null) {
+    // 로그인 페이지로
+  }
+  return <ListContainer />
 }
